5ed8072497e4f5b986148dcedc5036e25d013166
golrokh <golrokh.fatemi15@gmail.com>
2024-10-22 21:23:26 +0200

    Add your commit message here

---
+++ routers/product-router.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const { asyncHandler } = require('../utils/async-handler');
+const { validator } = require('../validations/validator');
+const {
+	addProduct,
+	getAllProducts,
+	getProductById,
+	editProductById,
+	removeProductById,
+	uploadProductImages
+} = require('../controllers/product-controller');
+const {
+	addProductValidationSchema,
+	editProductValidationSchema
+} = require('../validations/product-validation');
+const {protect,restrictTo} =require('../controllers/auth-controller')
+router.get('/', asyncHandler(getAllProducts));
+
+router.post(
+	'/',
+	protect,restrictTo("ADMIN"),
+	uploadProductImages,
+	validator(addProductValidationSchema),
+	asyncHandler(addProduct)
+);
+
+router.get('/:id', asyncHandler(getProductById));
+
+router.patch(
+	'/:id',
+	protect,restrictTo("ADMIN"),
+	uploadProductImages,
+	validator(editProductValidationSchema),
+	asyncHandler(editProductById)
+);
+
+router.delete('/:id',protect,restrictTo("ADMIN"), asyncHandler(removeProductById));
+
+module.exports = router;
